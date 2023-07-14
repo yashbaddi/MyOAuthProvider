2262c128bb316db8dc3606b48376e657d4124d9b
Yash Baddi <yashbaddi29@gmail.com>
2023-07-14 13:54:55 +0530

    Added Auth Middleware

---
+++ AuthorizationServer/middlewares/auth.js
@@ -0,0 +1,11 @@
+export function isAuthenticated(req, res, next) {
+  console.log(req.cookies);
+  if (
+    req.cookies.sessionID &&
+    sessions[req.cookies.sessionID] === req.cookies.username
+  ) {
+    next();
+  } else {
+    res.status(401).json({ message: "Unauthorized" });
+  }
+}
