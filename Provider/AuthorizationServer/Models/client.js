a11485f2dbb3e380852d51c9b98ce9987043d5b1
Yash Baddi <yashbaddi29@gmail.com>
2023-07-20 22:26:12 +0530

    ✨ADDED:Clients Model

--- Provider/AuthorizationServer/Models/client.js
+++ Provider/AuthorizationServer/Models/client.js
@@ -1,21 +1,21 @@
 import client from "./db-connection.js";
 
-async function readDB(id) {
+async function readClientDB(id) {
   const data = await client.get("client:" + id);
   return JSON.parse(data);
 }
 
-async function createDB(id, data) {
+async function createClientDB(id, data) {
   const data = await client.set("client:" + id, JSON.stringify(data));
   console.log("createDB data", data);
 }
 
-async function updateDB(id, data) {
+async function updateClientDB(id, data) {
   const data = await client.set("client:" + id, JSON.stringify(data));
   console.log("createDB data", data);
 }
 
-async function deleteDB(id) {
+async function deleteClientDB(id) {
   const data = await client.del("client:" + id);
   console.log(data);
 }
