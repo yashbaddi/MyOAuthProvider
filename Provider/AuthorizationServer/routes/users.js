7b9fae5b416277a24acabe30fbeadcbaa2738085
Yash Baddi <yashbaddi29@gmail.com>
2023-07-24 07:34:29 +0530

    🔨CLEANED:Authorization Server controllers

--- Provider/AuthorizationServer/routes/users.js
+++ Provider/AuthorizationServer/routes/users.js
@@ -3,18 +3,6 @@ import { createUser } from "../controllers/users.js";
 
 const usersRouter = express.Router();
 
-export const users = {
-  yashbaddi: {
-    password: "12345",
-    profile: {
-      name: "Yash Baddi",
-      email: "yashbaddi29@gmail.com",
-      data: "Hey this is the secret data",
-    },
-  }, //Test user
-};
-export const sessions = {};
-
 usersRouter.post("/", createUser);
 
 export default usersRouter;
