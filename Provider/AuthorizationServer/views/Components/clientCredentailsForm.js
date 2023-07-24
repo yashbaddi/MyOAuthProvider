cf65d7153c8d9ee0dc47b48fb2826eb71f5a680a
Yash Baddi <yashbaddi29@gmail.com>
2023-07-24 07:28:33 +0530

    🔨FIXED:Authorization Server consent page

--- Provider/AuthorizationServer/views/Components/clientCredentailsForm.js
+++ Provider/AuthorizationServer/views/Components/clientCredentailsForm.js
@@ -1,13 +1,47 @@
-export function clientCredentialsForm(root, credentials) {
+import { generateClientCredentialsRequest } from "../requests.js";
+import { clientCredentials } from "./clientCredentials.js";
+
+export function clientCredentialsForm(root) {
   root.innerHTML = "";
-  const clientCredDiv = document.createElement("div");
-  const clientID = document.createElement("p");
-  const clientSecretKey = document.createElement("p");
+  const clientCredentailsFormDiv = document.createElement("div");
+  const applicationName = document.createElement("input");
+  const redirectURI = document.createElement("input");
+  const grantType = document.createElement("input");
+  const scope = document.createElement("input");
+  const createClientBtn = document.createElement("button");
 
-  clientID.innerText = "Your Client ID is " + credentials.id;
-  clientSecretKey.innerText = "Your Client Secret is " + credentials.secret;
+  applicationName.type = "text";
+  applicationName.placeholder = "Application Name";
 
-  clientCredDiv.append(clientID, clientSecretKey);
+  redirectURI.type = "text";
+  redirectURI.placeholder = "Redirect URI";
 
-  root.append(clientCredDiv);
+  grantType.type = "text";
+  grantType.placeholder = "Grant Type";
+
+  scope.type = "text";
+  scope.placeholder = "Scope";
+
+  clientCredentailsFormDiv.append(
+    applicationName,
+    redirectURI,
+    grantType,
+    scope,
+    createClientBtn
+  );
+
+  createClientBtn.addEventListener("click", (e) => {
+    generateClientCredentialsRequest(
+      applicationName.value,
+      redirectURI.value,
+      grantType.value,
+      scope.value
+    ).then((res) => {
+      if (res[0]) {
+        clientCredentials(root, res[1]);
+      }
+    });
+  });
+
+  root.append(clientCredentailsFormDiv);
 }
